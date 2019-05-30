3303084c54d4d7561afb7f23e718fa14660d57f5
D.A. Rathnasiri <46153112+DARathnasiri@users.noreply.github.com>
2019-05-30 07:31:39 +0530

    Update postfeede.php

--- myWeb/postfeede.php
+++ myWeb/postfeede.php
@@ -1,13 +1,8 @@
 <?php
 session_start();
 require_once('include/index.php');
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myweb";
-
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli("localhost", "root", "", "myweb");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
