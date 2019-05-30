8aa9cf4341406e9d9dee328b82b99f0b0e004bd6
D.A. Rathnasiri <46153112+DARathnasiri@users.noreply.github.com>
2019-05-30 07:22:19 +0530

    Update firstpage.php

--- myWeb/firstpage.php
+++ myWeb/firstpage.php
@@ -15,9 +15,6 @@
 		<img src="assets/css/logo.png">
 
 	</div>
-	<div class="btn1">
-<button type="button" onclick="window.location.href='employee.php'">Continue as Employee</button>
-</div>
 <div class="btn2">
 <button type="button" onclick="window.location.href='customer.php'">Continue as Customer</button>
 	</div>
