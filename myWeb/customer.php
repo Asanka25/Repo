0acd1a9624bcd6949b265b5f8641f74ebd116916
JanakaShamal <47146026+JanakaShamal@users.noreply.github.com>
2019-05-30 08:29:17 +0530

    Update customer.php
    Add icons

--- myWeb/customer.php
+++ myWeb/customer.php
@@ -3,7 +3,10 @@
     <head>
       <meta charset="UTF-8">
       <title>Continue as Customer</title>
-      
+      link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+     <link rel="stylesheet"
+          href= 
+"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
       <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
           <link rel="stylesheet" href="./assets/css/style.css">
       
